82b8ab3defe71ad6c3ec673f661ff77b86d6067a
Pushkara Sharma <pushkarasharma@PushkaraMac.local>
2023-08-29 13:53:10 +0530

    dropdown position issue resolved

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useRef, useState } from 'react';
+import React, { FC, ReactElement, useEffect, useRef, useState } from 'react';
 import {
   FlatList,
   StyleSheet,
@@ -6,7 +6,11 @@ import {
   TouchableOpacity,
   Modal,
   View,
+  Dimensions,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
 } from 'react-native';
+import { GlobalColors } from '../Styles/GlobalStyleConfigs';
 
 interface Props {
   data: string[];
@@ -16,23 +20,48 @@ interface Props {
   setX?: number
 }
 
-const Dropdown: FC<Props> = ({ data, onSelect, renderContent, optionWidth, setX=100 }) => {
+const Dropdown: FC<Props> = ({ data, onSelect, renderContent, optionWidth, setX = 100 }) => {
   const DropdownButton = useRef(null);
+  const DropdownList = useRef(null);
+  const MAX_DROPDOWN_HEIGHT = 250;
+  const MAX_ELEMENT_HEIGHT = 40;
   const [visible, setVisible] = useState(false);
   const [dropdownTop, setDropdownTop] = useState(0);
   const [dropdownLeft, setDropdownLeft] = useState(0);
-  
+  const [windowHeight, setWindowHeight] = useState(Dimensions.get('window').height);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+
+  useEffect(() => {
+    const onLayout = () => { setWindowHeight(Dimensions.get('window').height); };
+    const subscription = Dimensions.addEventListener('change', onLayout);
+    return () => { subscription.remove(); };
+  }, []);
 
   const toggleDropdown = (): void => {
     visible ? setVisible(false) : openDropdown();
   };
 
   const openDropdown = (): void => {
-    DropdownButton.current.measure((_fx, _fy, _w, h, _px, py) => {
-      setDropdownTop(py + h);
-      setDropdownLeft(_px + _w - setX);
+    DropdownButton.current.measure((_fx, fy, _w, h, px, py) => {
+      const itemCount = data.length;
+      const dropdownHeight = Math.min(itemCount * MAX_ELEMENT_HEIGHT, MAX_DROPDOWN_HEIGHT);
+      const spaceBelow = py + h + dropdownHeight <= windowHeight;
+      const spaceAbove = py - dropdownHeight >= 0;
+
+      if (spaceBelow || (scrollPosition > 0 && spaceAbove)) {
+        setDropdownTop(py + h);
+      } else {
+        setDropdownTop(py - dropdownHeight);
+      }
+
+      setDropdownLeft(px + _w - setX);
     });
     setVisible(true);
+  };
+
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    setScrollPosition(event.nativeEvent.contentOffset.y);
   };
 
   const onItemPress = (item: any, index: number): void => {
@@ -41,29 +70,36 @@ const Dropdown: FC<Props> = ({ data, onSelect, renderContent, optionWidth, setX=
   };
 
   const renderItem = ({ item, index }: any) => (
-    <TouchableOpacity style={[styles.item, optionWidth ? {width: optionWidth}: {}]} onPress={() => onItemPress(item, index)}>
-      <Text style={{textTransform: 'capitalize'}}>{item}</Text>
+    <TouchableOpacity style={[styles.item, optionWidth ? { width: optionWidth } : {}]} onPress={() => onItemPress(item, index)}>
+      <Text style={{ textTransform: 'capitalize' }}>{item}</Text>
     </TouchableOpacity>
   );
 
   const renderDropdown = () => {
     return (
       <Modal visible={visible} transparent animationType="none">
-        <TouchableOpacity
-          style={styles.overlay}
-          onPress={() => setVisible(false)}
-        >
-          <View style={[styles.dropdown, { top: dropdownTop, left: dropdownLeft }]}>
+        <TouchableOpacity style={styles.overlay} onPress={() => setVisible(false)}>
+          <View
+            style={[
+              styles.dropdown,
+              { top: dropdownTop, left: dropdownLeft, maxHeight: MAX_DROPDOWN_HEIGHT },
+            ]}
+          >
             <FlatList
               data={data}
               renderItem={renderItem}
               keyExtractor={(item, index) => index.toString()}
+              style={{ maxHeight: MAX_DROPDOWN_HEIGHT }}
+              onScroll={handleScroll}
+              ref={DropdownList}
+              showsVerticalScrollIndicator={true}
             />
           </View>
         </TouchableOpacity>
       </Modal>
     );
   };
+
 
   return (
     <TouchableOpacity
@@ -78,6 +114,7 @@ const Dropdown: FC<Props> = ({ data, onSelect, renderContent, optionWidth, setX=
 
 const styles = StyleSheet.create({
   dropdown: {
+    paddingVertical: 5,
     position: 'absolute',
     backgroundColor: '#fff',
     shadowColor: '#000000',
@@ -93,7 +130,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 10,
     borderBottomWidth: 0.5,
-    borderColor: 'gray'
+    borderColor: GlobalColors.blue
   },
 });
 
